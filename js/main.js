bde54ebe0aab93d13d976b5e302dd8af7f81506a
Lucas villalba <lucas.m.albrigi@gmail.com>
2023-09-04 14:50:53 -0300

    primera pre entrega

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+const bienvenido = prompt("¿Bienvenido, listo para jugar?") + "\s" + "\n";
+ + "- Si" + "\s"
+ + "- No" + "\n"
+
+const pregunta ("¿Cual es la ciudad natal de Marco Polo?");
+const respuesta ("Venecia");
++ "a" + "venecia" + "\n";
++ "b" + "verona" + "\n";
++ "c" + "turin" + "\n";
++ "d" + "milan" + "\n";
+
+if (venecia){
+    console.log ("respuesta correcta");
+}
+else if (verona){
+    alert ("respuesta incorrecta");
+}
+else if (turin){
+    alert ("respuesta incorrecta");
+}
+else if (milan){
+    alert ("respuesta incorrecta");
+}
+
+while (siguiente pregunta == true);
+
+const pregunta ("¿que jugador marco el golas mas rapido de la historia de un mundial?");
+const respuesta ("Hakan Sukur (Turquía)");
++ "a" + "Hakan Sukur (Turquía)" + "\n";
++ "b" + "Diego Maradona (Argentina)" + "\n";
++ "c" + "Adalbert Desu (Rumanía)" + "\n";
++ "d" + "Edson Pele (Brasil)" + "\n";
+
+if (Hakan Sukur){
+    console.log ("respuesta correcta");
+}
+else if (verona){
+    alert ("respuesta incorrecta");
+}
+else if (turin){
+    alert ("respuesta incorrecta");
+}
+else if (milan){
+    alert ("respuesta incorrecta");
+}
+/*preguntar al jugador si desea seguir jugando*/
+
