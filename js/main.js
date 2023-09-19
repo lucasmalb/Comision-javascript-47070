10371c1b964338b02b58298425a43eaccb86e1a2
Lucas villalba <lucas.m.albrigi@gmail.com>
2023-09-19 12:22:52 -0300

    2da pre entrega

--- js/main.js
+++ js/main.js
@@ -5,224 +5,102 @@ Tutor: Facundo Casal*/
 
 
 
-
-
-
-let nombre
-let apellido
-let puntuacion = 0
-let bienvenido
-
-
-
-bienvenido = prompt('Bienvenido al juego!' + '\n' + '¿quiere empezar a jugar?' + '\n'
-    + 'si' + '\n'
-    + 'no' + '\n');
-    nombre = prompt('¿indique su nombre?');
-    apellido = prompt('¿indique su apellido');
-    
-function saludar(nombre, apellido) {
-    alert('hola ' + nombre + ' ' + apellido);
-}
-saludar(nombre, apellido);
-if (bienvenido == 'si') {
-
-
-    alert('para seleccionar la respuesta, ingrese el numero de la respuesta.')
-
-    do {
-        let respuesta
-        do {
-            respuesta = prompt('¿Que cargos ocupo Juan Domingo Peron ?' + '\n'
-                + '1 - presidente' + '\n'
-                + '2 - ministro de guerra' + '\n'
-                + '3 - secretario de trabajo' + '\n'
-                + '4 - todos');
-        } while (
-            respuesta != '1' &&
-            respuesta != '2' &&
-            respuesta != '3' &&
-            respuesta != '4'
-        )
-
-        if (respuesta === '4') {
-            puntuacion += 1
-            console.log('respuesta correcta');
+        let puntuacion = 0;
+        let respuesta;
+        function saludar(nombre, apellido) {
+            alert('Hola ' + nombre + ' ' + apellido);
         }
-        else {
-            alert('respuesta incorrecta');
-        };
-        do {
-            respuesta = prompt('¿Cuándo se creó la ONU?' + '\n'
-                + '1 - 1918' + '\n'
-                + '2 - 1945' + '\n'
-                + '3 - 1989' + '\n'
-                + '4 - 1978' + '\n');
-            if (respuesta === 2) {
-                puntuacion += 1
-                console.log('respuesta correcta');
-            }
-            else {
-                alert('respuesta incorrecta');
+        
+        function hacerPregunta(pregunta, respuestas, respuestaCorrecta, verificarRespuesta) {
+            
+            do {
+                respuesta = prompt(pregunta + '\n' + respuestas.join('\n'));
+            } while (!respuestaincludes(respuestas, respuesta));
+        
+            if (verificarRespuesta(respuesta, respuestaCorrecta)) {
+                console.log('Respuesta correcta');
+                return 1;
+            } else {
+                alert('Respuesta incorrecta');
+                return 0;
             }
         }
-        while (
-            respuesta != '1' &&
-            respuesta != '2' &&
-            respuesta != '3' &&
-            respuesta != '4'
-        )
-        do {
-            respuesta = prompt('¿Que jugador marco el golas mas rapido de la historia de un mundial?' + '\n'
-                + '1 - Hakan Sukur (Turquía)' + '\n'
-                + '2 - Diego Maradona (Argentina)' + '\n'
-                + '3 - Adalbert Desu (Rumanía)' + '\n'
-                + '4 - Edson Pele (Brasil)' + '\n');
-            if (respuesta === '1') {
-                puntuacion += 1
-                console.log('respuesta correcta');
+        function respuestaincludes (respuestas, respuesta){
+            let respuestavalida = false;
+            for (let i = 0; i< respuestas.length; i++){
+                if (respuesta [i].includes(respuesta)) {
+                    respuestavalida = true;
+                    break
+                }
             }
-            else {
-                alert('respuesta incorrecta');
+            console.log (respuestavalida);
+            return respuestavalida; 
+        
+        }
+        // Función de orden mayor para verificar respuestas
+        function verificarRespuesta(respuesta, respuestaCorrecta) {
+            return respuesta === respuestaCorrecta;
+        }
+        
+        const bienvenido = prompt('Bienvenido al juego!\n ¿Quieres empezar a jugar? \n (si/no)');
+        if (bienvenido === 'si') {
+            const nombre = prompt('Indique su nombre');
+            const apellido = prompt('Indique su apellido');
+            saludar(nombre, apellido);
+        
+            const preguntas = [
+                {
+                    pregunta: '¿Quién fue Perón?',
+                    respuestas: ['1 - Presidente', '2 - Ministro de Guerra', '3 - Secretario de Trabajo', '4 - Todos'],
+                    respuestaCorrecta: '4'
+                },
+                {
+                    pregunta: '¿Cuándo se creó la ONU?',
+                    respuestas: ['1 - 1918', '2 - 1945', '3 - 1989', '4 - 1978'],
+                    respuestaCorrecta: '2'
+                },
+                // Otras preguntas aquí...
+            ];
+        
+            let preguntaActual = 0; // Variable para llevar el seguimiento de la pregunta actual
+        
+            while (preguntaActual < preguntas.length) {
+                puntuacion += hacerPregunta(preguntas[preguntaActual].pregunta, preguntas[preguntaActual].respuestas, preguntas[preguntaActual].respuestaCorrecta, verificarRespuesta);
+                preguntaActual++; // Avanzar a la siguiente pregunta
             }
-        }
-        while (
-            respuesta != '1' &&
-            respuesta != '2' &&
-            respuesta != '3' &&
-            respuesta != '4'
-        )
-        do {
-            respuesta = prompt('¿Qué célula se encuentra tanto en humanos como en animales?' + '\n'
-                + '1 - corazon' + '\n'
-                + '2 - querantinocito' + '\n'
-                + '3 - neumocitos' + '\n'
-                + '4 - neurona' + '\n');
-            if (respuesta === '4') {
-                puntuacion += 1
-                console.log('respuesta correcta');
+        
+            switch (puntuacion) {
+                case 0:
+                    alert(`Tu puntuación fue de ${puntuacion}/5. Falta estudiar.`);
+                    break;
+                case 1:
+                    alert(`Tu puntuación fue de ${puntuacion}/5. Falta práctica.`);
+                    break;
+                case 2:
+                    alert(`Tu puntuación fue de ${puntuacion}/5. Sigue practicando.`);
+                    break;
+                case 3:
+                    alert(`Tu puntuación fue de ${puntuacion}/5. Regular.`);
+                    break;
+                case 4:
+                    alert(`Tu puntuación fue de ${puntuacion}/5. Casi lo logras.`);
+                    break;
+                case 5:
+                    alert(`Tu puntuación fue de ${puntuacion}/5. ¡Felicitaciones, has completado el juego!`);
+                    break;
             }
-            else {
-                alert('respuesta incorrecta');
+        
+            if (puntuacion !== 5) {
+                const continuar = prompt('¿Deseas jugar de nuevo? (si/no)');
+                if (continuar === 'si') {
+                    puntuacion = 0; // Reiniciar la puntuación
+                } else {
+                    alert('¡Hasta la próxima!');
+                }
+            } else {
+                alert('¡Hasta la próxima!');
             }
+        } else {
+            alert('Será la próxima vez. ¡Hasta luego!');
+        
         }
-        while (
-            respuesta != '1' &&
-            respuesta != '2' &&
-            respuesta != '3' &&
-            respuesta != '4'
-        )
-
-        do {
-            respuesta = prompt('¿Cual es la capital de Noruega?' + '\n'
-                + '1 - Helsinski' + '\n'
-                + '2 - Oslo' + '\n'
-                + '3 - Dublin' + '\n'
-                + '4 - Berlin' + '\n');
-            if (respuesta === '2') {
-                puntuacion += 1
-                console.log('respuesta correcta');
-            }
-            else {
-                alert('respuesta incorrecta');
-            }
-        }
-        while (
-            respuesta != '1' &&
-            respuesta != '2' &&
-            respuesta != '3' &&
-            respuesta != '4'
-        )
-        do {
-            respuesta = prompt('¿Cuanto es 18+(8x0)x120?' + '\n'
-                + '1 - 18' + '\n'
-                + '2 - 0' + '\n'
-                + '3 - 216' + '\n'
-                + '4 - 178' + '\n');
-            if (respuesta === '1') {
-                puntuacion += 1
-                console.log('respuesta correcta');
-            }
-            else {
-                alert('respuesta incorrecta');
-            }
-        }
-        while (
-            respuesta != '1' &&
-            respuesta != '2' &&
-            respuesta != '3' &&
-            respuesta != '4'
-        )
-        switch (puntuacion) {
-            case 0:
-                alert(`tu puntuacion fue de ${puntuacion}/5. falta estudiar!`);
-                break;
-            case 1:
-                alert(`tu puntuacion fue de ${puntuacion}/5. falta practica`);
-                break;
-            case 2:
-                alert(`tu puntuacion fue de ${puntuacion}/5. segui practicando`);
-                break;
-            case 3:
-                alert(`tu puntuacion fue de ${puntuacion}/5. regular`);
-                break;
-            case 4:
-                alert(`tu puntuacion fue de ${puntuacion}/5. casilo logras!`);
-                break;
-            case 5:
-                alert(`tu puntuacion fue de ${puntuacion}/5. felicitaciones, has completado el juego!`);
-                break;
-        }
-        if (puntuacion = 5) {
-            continuar = 'no'
-
-        }
-        else {
-            continuar = prompt('¿deseas jugar de nuevo? si/no');
-        }
-
-
-    }
-    while (continuar == 'si');
-} else {
-    alert('sera la proxima!');
-}
-
-
-/*const pregunta1 = new preguntas
-const pregunta2 = pregunta;
-const pregunta3 = pregunta;
-const pregunta4 = pregunta;
-const pregunta5 = pregunta;
-/*const nombre = lucas
-const apellido = villalba
-const puntuacion = 0
-const bienvenido = prompt('Bienvenido al juego!' + '\n' + '¿quiere empezar a jugar?' + '\n'
-+ 'si' + '\n'
-+ 'no' + '\n');
-nombre = prompt('¿indique su nombre?');
-apellido = prompt('¿indique su apellido');
-function saludar (nombre, apellido){
-    alert ('hola ' + nombre + ' ' + apellido);
-}
-saludar(nombre, apellido);
-if (bienvenido == 'si') 
-
-        function preguntas (pregunta, respuesta1, respuesta2, respuesta3, respuesta4){
-            this.pregunta = pregunta,
-            this.respuesta1 = respuesta1,
-            this.respuesta2 = respuesta2,
-            this.respuesta3 = respuesta3,
-            this.respuesta4 = respuesta4
-        }
-        console.log (preguntas);
-        const pregunta1 = new preguntas ('¿que cargos ocupo juan domingo peron?', 'a- secretario de trabajo', 'b- presidente', 'c- ministro de guerra', 'd- todos')
-        console.log (pregunta1) 
-        const pregunta2 = new preguntas ('¿Cuándo se creó la ONU?', 1918, 1945, 1989, 1978)
-        console.log (pregunta2)
-        const pregunta3 = new preguntas ('¿Que jugador marco el golas mas rapido de la historia de un mundial?', ' Hakan Sukur (Turquía)', 'Diego Maradona (Argentina)', ' Adalbert Desu (Rumanía)', 'Edson Pele (Brasil)')
-        console.log (pregunta3)
-        const pregunta4 = new preguntas ('¿Qué célula se encuentra tanto en humanos como en animales?', 'corazon', ' querantinocito', 'neumocitos', 'neurona')
-        console.log (pregunta4)
-        const pregunta5 = new preguntas ('¿Cual es la capital de Noruega?', 'Oslo', 'Berlin', 'Hensiski', 'kiev')
-        console.log (pregunta5)*/
